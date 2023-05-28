21585f4120a4c925350a97e8ec6554c1d1e0a37e
vt415204 <ramon.pinho@vtal.com>
2023-05-28 12:34:58 +0000

    feat: update puppeteer params

--- src/Login.js
+++ src/Login.js
@@ -8,10 +8,11 @@ async function Login() {
     const browser = await puppeteer.launch({
       headless: true,
       args: [
-        "--start-maximized", // you can also use '--start-fullscreen'
+	"--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
       ],
-      defaultViewport: null,
-      browserContext: "default",
     });
 
     const page = await browser.newPage();
