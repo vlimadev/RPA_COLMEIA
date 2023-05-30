7d2f0639fb9a00e9e306cacd30fa0cc1b9af9c74
Ramon Pinho <ramonuchoa386@gmail.com>
2023-05-30 06:37:49 -0300

    feat: update poppeteer launch config options; add defaultViewport

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,56 @@
-require("dotenv").config();
-const puppeteer = require("puppeteer");
-const path = require("path");
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+const path = require('path');
 
 async function Login() {
-  try {
-    //Inicia o navegador e direciona para a comeia
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-	"--disable-gpu",
-        "--disable-dev-shm-usage",
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-      ],
-    });
+	const args =
+		process.env.NODE_ENV === 'production'
+			? [
+					'--disable-gpu',
+					'--disable-dev-shm-usage',
+					'--disable-setuid-sandbox',
+					'--no-sandbox',
+			  ]
+			: ['--start-maximized'];
 
-    const page = await browser.newPage();
-    await page.goto("https://app.colmeia.me/auth/signIn");
+	const launchOptions = {
+		args: args,
+		defaultViewport: {
+			width: 1366,
+			height: 768,
+		},
+	};
 
-    const client = await page.target().createCDPSession();
-    await client.send("Page.setDownloadBehavior", {
-      behavior: "allow",
-      downloadPath: path.resolve(__dirname, "../uploads"),
-    });
+	if (process.env.NODE_ENV !== 'production') {
+		Object.assign(launchOptions, {
+			devtools: true,
+			slowMo: Number(process.env.ENV_SLOW_PUPPETEER || 1000),
+		});
+	}
 
-    //Insere as credencias de login
-    await page.type('[id="mat-input-0"]', process.env.COLMEIA_EMAIL);
-    await page.type('[id="mat-input-1"]', process.env.COLMEIA_SENHA);
-    await page.click('[class="landing-main-button"]');
-    console.log("Fez login");
+	try {
+		//Inicia o navegador e direciona para a comeia
+		const browser = await puppeteer.launch(launchOptions);
 
-    return { data: "", message: "OK", contextPage: page };
-  } catch (error) {
-    return { data: error, message: "ERROR" };
-  }
+		const page = await browser.newPage();
+		await page.goto('https://app.colmeia.me/auth/signIn');
+
+		const client = await page.target().createCDPSession();
+		await client.send('Page.setDownloadBehavior', {
+			behavior: 'allow',
+			downloadPath: path.resolve(__dirname, '../uploads'),
+		});
+
+		//Insere as credencias de login
+		await page.type('[id="mat-input-0"]', process.env.COLMEIA_EMAIL);
+		await page.type('[id="mat-input-1"]', process.env.COLMEIA_SENHA);
+		await page.click('[class="landing-main-button"]');
+		console.log('Fez login');
+
+		return { data: '', message: 'OK', contextPage: page };
+	} catch (error) {
+		return { data: error, message: 'ERROR' };
+	}
 }
 
 module.exports = Login;
